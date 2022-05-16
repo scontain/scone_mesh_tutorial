43e122cbe7b801af12f906fa2c473da6987ad3cc
Christof Fetzer <christof.fetzer@scontain.com>
2022-05-16 09:53:08 +0200

    added one more check

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-set -x -e
-
+set -e  -x
 
 # ensure we have access to sconectl
 
@@ -17,10 +16,28 @@ then
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     else
         echo "Ensuring Rust is up to date"
+        rustup update
     fi
 
     cargo install sconectl
 fi
+
+# ensure we have access to docker
+
+if ! command -v docker &> /dev/null
+then
+    echo "No docker found! You need to install docker or podman. EXITING."
+    exit 1
+fi
+
+# ensure we have access to the base container image
+
+docker pull registry.scontain.com:5050/cicd/sconecli:latest 2> /dev/null || { 
+    echo "You must get access to image `cicd/sconecli:latest`." 
+    echo "Please send email info@scontain.com to ask for access"
+    exit 1
+}
+
 
 # build service image
 #  - if the push fails, add --no-push or change the TO field
