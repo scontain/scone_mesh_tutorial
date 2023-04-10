094b684ce84a8734bf39b590d68998a3bc9c7dae
Karin Fetzer <karin.fetzer@scontain.com>
2023-04-10 20:36:45 +0200

    Add namespace to k get pod requests

--- check_pods.sh
+++ check_pods.sh
@@ -11,8 +11,8 @@ if [[ "$RELEASE" == "" ]]; then
     RELEASE="mesh-tutorial"
 fi
 
-export POD=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=$APP_NAME,app.kubernetes.io/instance=$RELEASE" -o jsonpath="{.items[0].metadata.name}")
-export READY=$(kubectl get pod $POD -o jsonpath='{.status.containerStatuses[0].ready}')
+export POD=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=$APP_NAME,app.kubernetes.io/instance=$RELEASE" -o jsonpath="{.items[0].metadata.name}" 2> /dev/null)
+export READY=$(kubectl get pod $POD -o jsonpath='{.status.containerStatuses[0].ready}' --namespace $NAMESPACE 2> /dev/null)
 
 echo "Checking ready status of pod $POD:"
 echo "  RELEASE:      $RELEASE"
@@ -29,8 +29,8 @@ while [[ "$READY" != "true"  ]] ; do
         exit 1
     fi
     sleep 5
-    export POD=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=$APP_NAME,app.kubernetes.io/instance=$RELEASE" -o jsonpath="{.items[0].metadata.name}")
-    export READY=$(kubectl get pod $POD -o jsonpath='{.status.containerStatuses[0].ready}')
+    export POD=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=$APP_NAME,app.kubernetes.io/instance=$RELEASE" -o jsonpath="{.items[0].metadata.name}" 2> /dev/null)
+    export READY=$(kubectl get pod $POD -o jsonpath='{.status.containerStatuses[0].ready}' --namespace $NAMESPACE 2> /dev/null)
     echo "  READY STATUS: $READY"
 done
 
